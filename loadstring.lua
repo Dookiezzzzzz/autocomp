fe52adda62ada6473244dc4ae8156bc0c310926c
RelapseDevX <callumlovesdad@gmail.com>
2025-04-29 21:56:59 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,4 +3,4 @@ _G.min_chance = 10
 _G.pingEveryone = "Yes" 
 _G.webhook = "https://discord.com/api/webhooks/1366984076434608129/PXVh0Z1gU0gi09OUkNhEugq5ByukXaqmt99sDLARQSbAPUrf8BZxcrvHBsdU-wA9-cUZ" 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VertexPeak/vertex/refs/heads/main/source.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RelapseDevX/RelapseHub/refs/heads/main/source.lua"))()
